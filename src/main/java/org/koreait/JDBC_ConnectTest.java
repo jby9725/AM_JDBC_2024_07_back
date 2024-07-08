2f1f4233b6ede88f08ecf1a716ba0541fc96038f
jby9725 <jby9725@gmail.com>
2024-07-08 14:36:18 +0900

    JDBC 연결 테스트클래스 추가

---
+++ src/main/java/org/koreait/JDBC_ConnectTest.java
@@ -0,0 +1,36 @@
+package org.koreait;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC_ConnectTest {
+
+    public static void main(String[] args) {
+        Connection conn = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+//            String url = "jdbc:mariadb://localhost:3306/ArticleManager";
+            //
+             String url = "jdbc:mariadb://127.0.0.1:3306/ArticleManager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+            conn = DriverManager.getConnection(url, "root", "");
+            System.out.println("연결 성공!");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("드라이버 로딩 실패" + e);
+        } catch (SQLException e) {
+            System.out.println("에러 : " + e);
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,5 +1,9 @@
 package org.koreait;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,39 +16,63 @@ public class Main {
     static List<Article> articles = new ArrayList<>();
     static int lastID = 0;
 
+    static String url = "jdbc:mariadb://localhost:3306/ArticleManager";
+    static String userName = "root";
+    static String password = "";
+
+
     public static void main(String[] args) {
 
-        while (sys_status == 0) {
-            System.out.print("명령어 > ");
-            cmd = scanner.nextLine().trim();
-
-            if (cmd.equals("exit")) {
-                sys_status = -1;
-                System.out.println("프로그램 종료");
-                continue;
+        try {
+            Connection conn = DriverManager.getConnection(url, userName, password);
+            if (conn != null) {
+                System.out.println("Connected to database");
             }
-            if (cmd.equals("article write")) {
-                System.out.print("제목 : ");
-                String title = scanner.nextLine();
-                System.out.print("내용 : ");
-                String body = scanner.nextLine();
-
-                Article newArticle = new Article(++lastID, title, body);
-
-                articles.add(newArticle);
-
-            } else if (cmd.equals("article list")) {
-                if (articles.size() == 0) {
-                    System.out.println("게시글 없음");
-                    continue;
-                }
-                System.out.println("   번호    /    제목     ");
-                for (Article article : articles) {
-                    System.out.printf("     %d      /    %s    \n", article.getId(), article.getTitle());
-                }
-            } else {
-                System.out.println("잘못된 명령어");
-            }
+        } catch (SQLException e) {
+            System.out.println("DB 접속 실패");
+            e.printStackTrace();
+//            throw new RuntimeException(e);
         }
+
+
+        /////////////////////////////////
+
+
+
+        /////////////////////////////////
+
+
+//        while (sys_status == 0) {
+//            System.out.print("명령어 > ");
+//            cmd = scanner.nextLine().trim();
+//
+//            if (cmd.equals("exit")) {
+//                sys_status = -1;
+//                System.out.println("프로그램 종료");
+//                continue;
+//            }
+//            if (cmd.equals("article write")) {
+//                System.out.print("제목 : ");
+//                String title = scanner.nextLine();
+//                System.out.print("내용 : ");
+//                String body = scanner.nextLine();
+//
+//                Article newArticle = new Article(++lastID, title, body);
+//
+//                articles.add(newArticle);
+//
+//            } else if (cmd.equals("article list")) {
+//                if (articles.size() == 0) {
+//                    System.out.println("게시글 없음");
+//                    continue;
+//                }
+//                System.out.println("   번호    /    제목     ");
+//                for (Article article : articles) {
+//                    System.out.printf("     %d      /    %s    \n", article.getId(), article.getTitle());
+//                }
+//            } else {
+//                System.out.println("잘못된 명령어");
+//            }
+//        }
     }
 }
